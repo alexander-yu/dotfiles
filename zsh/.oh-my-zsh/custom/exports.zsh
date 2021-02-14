37e34d3ac07c0848e578700e437a70f015e6d84d
Alex Yu <yu.alex96@gmail.com>
2021-02-14 16:16:48 -0500

    add gopath

--- zsh/.oh-my-zsh/custom/exports.zsh
+++ zsh/.oh-my-zsh/custom/exports.zsh
@@ -5,6 +5,9 @@ export PATH=$PATH:$HOME/go/bin
 # Python
 export PYTHONSTARTUP="$HOME/.pythonstartup"
 
+# Golang
+export GOPATH=$GOPATH:$HOME/go
+
 # bug fix: see https://github.com/sorin-ionescu/prezto/issues/966#issuecomment-172003005
 export PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
 export MANPATH="/usr/local/opt/coreutils/libexec/gnuman:$MANPATH"
