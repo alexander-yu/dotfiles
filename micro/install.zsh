89f2ed037ac545ff0bccb25e5f6c59b30f07d73a
Alex Yu <yu.alex96@gmail.com>
2022-03-07 03:39:12 -0500

    [micro] Update

--- micro/install.zsh
+++ micro/install.zsh
@@ -1,9 +1,6 @@
 (
     set -eo pipefail
 
-    # Symlink micro colorschemes
-    ln -s "$(readlink -f $(dirname $0))/colorschemes/*.micro" "$HOME/.config/micro/"
-
     # Symlink micro config
     ln -s "$(readlink -f $(dirname $0))/settings.json" "$HOME/.config/micro/settings.json"
 )
