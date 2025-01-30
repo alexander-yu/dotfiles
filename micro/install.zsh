d5b93c13608f42fa58bea33b5f97785d2e0714eb
Alex Yu <yu.alex96@gmail.com>
2025-01-30 13:22:36 -0500

    Micro keybindings

--- micro/install.zsh
+++ micro/install.zsh
@@ -3,4 +3,6 @@
 
     # Symlink micro config
     ln -s "$(git rev-parse --show-toplevel)/micro/settings.json" "$HOME/.config/micro/settings.json"
+
+    ln -s "$(git rev-parse --show-toplevel)/micro/bindings.json" "$HOME/.config/micro/bindings.json"
 )
