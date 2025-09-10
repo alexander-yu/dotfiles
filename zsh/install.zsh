fa0046a753a4b6e60cd5cd6b920a944ecceb0870
Alex Yu <ayu@hioscar.com>
2025-09-10 16:17:19 -0400

    fix zsh install

--- zsh/install.zsh
+++ zsh/install.zsh
@@ -11,12 +11,12 @@
             $(brew --prefix)/opt/fzf/install
         ;;
         Linux)
-            nix-env -if "$dir/zsh/pkgs.nix"
+            nix-env -if "$dir/pkgs.nix"
         ;;
     esac
 
     # Install oh-my-zsh if it's not configured
-    if [[ -n $ZSH  ]]; then
+    if [[ -z $ZSH  ]]; then
         sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
     fi
 
@@ -27,12 +27,11 @@
     ln -sf "$dir/.zshrc" "$HOME/.zshrc"
 
     # Symlink oh-my-zsh custom files
-    ln -s "$dir"/.oh-my-zsh/custom/* "$ZSH/custom"
+    ln -sf "$dir"/.oh-my-zsh/custom/* "$ZSH/custom"
 
     # Symlink powerlevel10k config
-    ln -s "$dir/.p10k.zsh" "$HOME/.p10k.zsh"
-    p10k configure
+    ln -sf "$dir/.p10k.zsh" "$HOME/.p10k.zsh"
 
     # Symlink iterm shell integration
-    ln -s "$dir/.iterm2_shell_integration.zsh" "$HOME/.iterm2_shell_integration.zsh"
+    ln -sf "$dir/.iterm2_shell_integration.zsh" "$HOME/.iterm2_shell_integration.zsh"
 )
